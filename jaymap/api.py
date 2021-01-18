f851775078a58d50f86b3547a1f60a69ce3a07ff
John Reese <john@noswap.com>
2021-01-17 21:32:37 -0800

    lint

--- jaymap/api.py
+++ jaymap/api.py
@@ -4,7 +4,7 @@
 from dataclasses import dataclass
 from typing import TYPE_CHECKING, Optional, Iterable
 
-from jaymap.types.core import Id, UnsignedInt, Capabilities
+from jaymap.types.core import Id
 
 if TYPE_CHECKING:
     from jaymap.client import JMAP
